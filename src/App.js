0438a02fb179151f4e9de672b3978a52eff50094
Cherish Collins <cherish.ebojudah@gmail.com>
2021-04-20 14:47:53 +0100

    set input field to empty after populating list, updated button type and added preventdefault function to app

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import React, { useState } from 'react'
 import './App.css';
 
 function App() {
-  const [todos, setTodos] = useState(['take dogs for a walk', 'take the rubbish out']);
+  const [todos, setTodos] = useState(['take dogs for a walk', 'take the rubbish out', 'cherish wants to learn react']);
+  const [input, setInput] = useState('');
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, input])
+    setInput('');
+  }
 
   return (
     <div className="App">
       <h1>Hello Clever Programmers</h1>
-      <input />
-      <button>Add Todo</button>
+      <form>
+        <input value={input} onChange={event => setInput(event.target.value)}/>
+        <button type='submit' onClick = {addTodo}>Add Todo</button>
+      </form>
 
       <ul>
         {todos.map(todo => (
