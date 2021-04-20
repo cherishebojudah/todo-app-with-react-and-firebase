a6b14d07399196ea614ab7073b25bfcf0f2e0b4b
Cherish Collins <cherish.ebojudah@gmail.com>
2021-04-20 05:19:09 +0100

    created react app todo_list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState(['take dogs for a walk', 'take the rubbish out']);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Clever Programmers</h1>
+      <input />
+      <button>Add Todo</button>
+
+      <ul>
+        <li></li>
+        <li></li>
+      </ul>
+      
     </div>
   );
 }
